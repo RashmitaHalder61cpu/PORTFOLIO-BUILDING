3b8e88c23e13461fc2f058f1eef39628a00d6719
RashmitaHalder61cpu <rashmitahalder61@gmail.com>
2026-09-03 15:36:44 +0530

    Add greet() function -paired with Prajwal via Live Share

---
+++ activity3/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void greet(const char *name)
+{
+    printf("Hello, %s! Welcome to your GitHub portfolio.\n", name);
+}
+
+int main()
+{
+    printf("Hello Rashmita");
+    greet("Prajwal");
+}
